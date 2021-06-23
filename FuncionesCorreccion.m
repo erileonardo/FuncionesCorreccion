a4b70a6d55ef871b050d2503e4abd3edad39ec5b
erileonardo <82676091+erileonardo@users.noreply.github.com>
2021-06-23 02:11:41 -0500

    Add files via upload

---
+++ FuncionesCorreccion.m
@@ -0,0 +1,23 @@
+% Octave Script
+% Title       : Funciones
+% Description : Graficar  
+% Author      : Erika Amairani Leonardo Arce
+% Date        : 23 de junio 2021
+% Version     : 1
+% Notes       : funcion f(x)=16-x^2/4-x
+
+
+clc
+
+%Grafica
+x=-5:0.1:-2;
+y=16-(x.^2)/4-x;
+% Dibujar grafica
+plot(x,y)
+grid on; 
+% Titulo
+title("f(x)=16-x^2/4-x");
+% Etiqueta para x
+xlabel("X");
+% Etiqueta para y
+ylabel("Y");
